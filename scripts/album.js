550d7b87fa79398b8bf63742726e274e57627bd9
Kjwolf78 <kjwolf78@gmail.com>
2017-07-06 15:39:11 -0400

    dom25assignment

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,21 @@ var albumMarconi = {
     ]
 };
 
+var albumKyle = {
+    title: 'The Coder',
+    artist: 'Kyle Wolf',
+    label: 'Bloc',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
       '<tr class="album-view-song-item">'
@@ -65,4 +80,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
+  var albums = [albumPicasso, albumMarconi, albumKyle];
+  var index = 1;
+  albumImage.addEventListener("Click", function(event) {
+    setCurrentAlbum(albums[index]);
+    index++;
+    if (index == album.length) {
+      index = 0;
+    }
+  });
+
 };
